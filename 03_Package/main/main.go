32ce49cd189ec924ddafcbec7bc6c235fa9a41d1
Jhoust <jaredbhouston@gmail.com>
2018-07-03 01:19:28 -0700

    Add files via upload

---
+++ 03_Package/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/NewbieCrit/Udemy-Training/03_Package/main/stringutil"
+)
+
+	func main() {
+
+	fmt.Println(stringutil.Reverse("uoy evol"))
+	fmt.Println(stringutil.MyName)
+
+
+
+
+}
