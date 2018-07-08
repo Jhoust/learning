7e2c8128112d74d4215b4f9c6a7c7a4a9efc8d8f
Jared <j.houst@hotmail.com>
2018-07-08 01:09:09 -0700

    adding lesson 8 memory address and first use of .scan

---
+++ 05_Visibility/scope/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+ var x = "boss hogg"
+
+func one() {
+	fmt.Println(x)
+}
+
+	func leftover() {
+		fmt.Println(x)
+	}
+
+		func
+		main() {
+			leftover()
+			one()
+		}
